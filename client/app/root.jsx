55a9e93a8c17bfc44602b4c30c6d807ee05bd56d
kich555 <hyunjjang199582@gmail.com>
2022-08-29 20:56:05 +0900

    Initial commit

---
+++ client/app/root.jsx
@@ -0,0 +1,36 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+import { MantineProvider } from '@mantine/core';
+import { StylesPlaceholder } from '@mantine/remix';
+
+export const meta = () => ({
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+});
+
+export default function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <html lang='en'>
+        <head>
+          <Meta />
+          <Links />
+          <StylesPlaceholder />
+        </head>
+        <body>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    </MantineProvider>
+  );
+}
