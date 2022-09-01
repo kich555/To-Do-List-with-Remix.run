102e3e7d7ef4b322205a47bd95e331e3bc618e5b
kich555 <hyunjjang199582@gmail.com>
2022-09-01 23:03:25 +0900

    setting entry.client to react v18

--- client/app/entry.client.jsx
+++ client/app/entry.client.jsx
@@ -1,10 +1,32 @@
-import { RemixBrowser } from '@remix-run/react';
-import { hydrate } from 'react-dom';
-import { ClientProvider } from '@mantine/remix';
+// import { RemixBrowser } from '@remix-run/react';
+// import { hydrate } from 'react-dom';
+// import { ClientProvider } from '@mantine/remix';
 
-hydrate(
-  <ClientProvider>
-    <RemixBrowser />
-  </ClientProvider>,
-  document
-);
+// hydrate(
+//   <ClientProvider>
+//     <RemixBrowser />
+//   </ClientProvider>,
+//   document
+// );
+
+import * as React from 'react';
+import { ClientProvider } from '@mantine/remix';
+import { RemixBrowser } from '@remix-run/react';
+import { hydrateRoot } from 'react-dom/client';
+
+function hydrate() {
+  React.startTransition(() => {
+    hydrateRoot(
+      document,
+      <ClientProvider>
+        <RemixBrowser />
+      </ClientProvider>
+    );
+  });
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  window.setTimeout(hydrate, 1);
+}
