30f384ffb033eef62bbd8d8feee24f76749685fe
kich555 <hyunjjang199582@gmail.com>
2022-09-03 17:18:47 +0900

    Create dnd TodoCategory component

---
+++ client/app/components/pages/todos/TodoCategory.jsx
@@ -0,0 +1,35 @@
+import { Title } from '@mantine/core';
+import { Droppable } from 'react-beautiful-dnd';
+import TodoCard from '~/components/pages/todos/TodoCard';
+
+export default function TodoCategory({ category, prefix }) {
+  console.log('category', category);
+  const cards = category.map((card, index) => (
+    <TodoCard key={card.name} card={card} index={index} />
+  ));
+
+  return (
+    <section
+      style={{
+        width: '280px',
+        padding: '0 20px',
+      }}
+    >
+      <Title align='center' mb={20} mt={10}>
+        {prefix}
+      </Title>
+      <Droppable droppableId={prefix} direction='vertical'>
+        {(provided) => (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            style={{ width: '100%', height: '100vh', overflowY: 'scroll' }}
+          >
+            {cards}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </section>
+  );
+}
