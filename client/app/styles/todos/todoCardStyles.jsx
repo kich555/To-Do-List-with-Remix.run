8bfbae64161d68cba4e65bd60e07602f9f64f04c
kich555 <hyunjjang199582@gmail.com>
2022-09-03 16:48:32 +0900

    Create dnd Sample Card styles

---
+++ client/app/styles/todos/todoCardStyles.jsx
@@ -0,0 +1,29 @@
+import { createStyles } from '@mantine/core';
+
+const todoCardStyles = createStyles((theme) => ({
+  item: {
+    ...theme.fn.focusStyles(),
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: theme.radius.md,
+    border: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+    padding: `${theme.spacing.sm}px ${theme.spacing.xl}px`,
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.white,
+    marginBottom: theme.spacing.sm,
+  },
+
+  itemDragging: {
+    boxShadow: theme.shadows.sm,
+  },
+
+  symbol: {
+    fontSize: 30,
+    fontWeight: 700,
+    width: 60,
+  },
+}));
+
+export default todoCardStyles;
