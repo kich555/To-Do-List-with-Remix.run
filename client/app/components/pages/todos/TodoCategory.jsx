8e3ea506f418f995dd02bfc3b224a62d72026349
kich555 <hyunjjang199582@gmail.com>
2022-09-04 19:16:10 +0900

    Create TodoCategory component

--- client/app/components/pages/todos/TodoCategory.jsx
+++ client/app/components/pages/todos/TodoCategory.jsx
@@ -1,50 +1,56 @@
-import { Box, Button, Container, ScrollArea, Text, Textarea, Title } from '@mantine/core';
+import { Box, Button, Container, Text, Textarea, Title } from '@mantine/core';
+import { useScrollIntoView } from '@mantine/hooks';
 import { useState } from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import TodoCard from '~/components/pages/todos/TodoCard';
-import todoCategoryStyles from '../../../styles/todos/todoCategoryStyles';
+import todoCategoryStyles from '~/styles/todos/todoCategoryStyles';
 
 export default function TodoCategory({ category, prefix }) {
   const [isClicked, setIsClicked] = useState(false);
   const { classes } = todoCategoryStyles();
-  const { button } = classes;
+  const { wrapper, buttonWrapper, createCardInput } = classes;
+  const { scrollIntoView, targetRef, scrollableRef } = useScrollIntoView({ duration: 0 });
 
   const cards = category.map((card, index) => <TodoCard key={card.name} card={card} index={index} />);
 
   return (
-    <Container size={300} px={20} m={0}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative', width: '100%', height: '100%' }}>
-        <Title align="center" mb={20}>
-          {prefix}
-        </Title>
-        <Droppable droppableId={prefix} direction="vertical">
-          {provided => (
-            <div {...provided.droppableProps} ref={provided.innerRef} style={{ width: '100%', overflowY: 'auto' }}>
+    <Container size={300} px={20} m={0} sx={{ width: '100%' }}>
+      <Droppable droppableId={prefix} direction="vertical">
+        {provided => (
+          <Box {...provided.droppableProps} ref={provided.innerRef}>
+            <Title align="center" mb={20}>
+              {prefix}
+            </Title>
+            <Box ref={scrollableRef} className={wrapper}>
               {cards}
               {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-        <Box sx={{ width: '100%' }}>
-          {isClicked ? (
-            <Box>
-              <Textarea placeholder="Enter a title for this card..." sx={{ width: '100%', height: '54px', margin: '10px 0', borderRadius: '4px' }} />
-              <Box mt={20} sx={{ display: 'flex', gap: '10px' }}>
-                <Button>Add Card</Button>
-                <Button color="red" onClick={() => setIsClicked(false)}>
-                  Cancel
-                </Button>
-              </Box>
+              <div ref={targetRef} />
             </Box>
-          ) : (
-            <Button onClick={() => setIsClicked(true)}>
-              <Text component="span" className={button}>
-                + Add a card
-              </Text>
-            </Button>
-          )}
-        </Box>
-      </Box>
+            <Box mt={20} sx={{ width: '100%' }}>
+              {isClicked ? (
+                <Box>
+                  <Textarea placeholder="Enter a title for this card..." className={createCardInput} />
+                  <Box mt={20} className={buttonWrapper}>
+                    <Button>Add Card</Button>
+                    <Button color="red" onClick={() => setIsClicked(false)}>
+                      Cancel
+                    </Button>
+                  </Box>
+                </Box>
+              ) : (
+                <Button
+                  onClick={() => {
+                    setIsClicked(true);
+                    scrollIntoView();
+                  }}
+                >
+                  <Text component="span">+ Add a card</Text>
+                </Button>
+              )}
+            </Box>
+          </Box>
+        )}
+      </Droppable>
     </Container>
   );
 }
