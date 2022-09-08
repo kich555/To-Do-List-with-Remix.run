a6f259441b843ce9e2999e053ba02d2fd79094b5
kich555 <hyunjjang199582@gmail.com>
2022-09-08 23:23:55 +0900

    Create updateTodos on todo model

--- client/app/models/todo.server.js
+++ client/app/models/todo.server.js
@@ -4,3 +4,13 @@ import { db } from '../utils/db.server'
 export async function getTodos() {
     return db.todo.findMany();
 }
+
+export async function updateTodos(todoList) {
+    const newTodos =  Object.values(todoList).reduce((acc,cur)=> {
+     return [...acc, ...cur]   
+    })
+console.log('newTodos',newTodos)
+    return db.todo.updateMany({
+        
+    })
+}
