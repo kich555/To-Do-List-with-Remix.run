e2b7bce257d2b14413890557d81deca8099c9b1f
kich555 <hyunjjang199582@gmail.com>
2022-09-12 02:32:51 +0900

    Redesign file structure on todos route

--- client/app/routes/todos.jsx
+++ client/app/routes/todos.jsx
@@ -1,9 +1,44 @@
-import { Link, Outlet } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
+import { json, redirect } from '@remix-run/node';
+import TodoList from '~/components/pages/todos/TodoList';
+import { createTodo, getTodos } from '~/models/todo.server';
 
-export default function TodoRoute() {
+export const loader = async () => {
+  const todos = await getTodos();
+
+  const newTodos = todos.reduce((acc, cur) => {
+    if (!acc[cur.progress]) {
+      acc[cur.progress] = [cur];
+    } else {
+      acc[cur.progress].push(cur);
+    }
+
+    return { ...acc };
+  }, {});
+  return json({ newTodos });
+};
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const title = formData.get('title');
+  const progress = formData.get('progress');
+  const category = formData.get('category');
+  const index = Number(formData.get('index'));
+
+  if (typeof title !== 'string' || title.length === 0) {
+    return json({ errors: { title: 'Title is required' } }, { status: 400 });
+  }
+
+  await createTodo({ title, progress, category, index });
+
+  return redirect('/todos');
+};
+
+export default function TodosIndexRoute() {
+  const { newTodos } = useLoaderData();
   return (
     <section style={{ height: '100vh' }}>
-      <Outlet />
+      <TodoList todos={newTodos} />
     </section>
   );
 }
