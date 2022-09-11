4ba3e28c8d09524ac1f9fb38c4eeda4bb208e245
kich555 <hyunjjang199582@gmail.com>
2022-09-12 02:34:28 +0900

    Create todo detail route file

--- client/app/routes/todos/$id.jsx
+++ client/app/routes/todos/$id.jsx
@@ -1,14 +1,7 @@
-import { Modal } from '@mantine/core';
-import { useState } from 'react';
-
 export default function $idRoute() {
-  const [opened, setOpened] = useState(true);
-
   return (
     <>
-      <Modal opened={opened} onClose={() => setOpened(false)} title="Introduce yourself!">
-        <div>asfasfsafas</div>
-      </Modal>
+      <div>asfasfsafas</div>
     </>
   );
 }
