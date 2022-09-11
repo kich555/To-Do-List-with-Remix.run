0e9143aedfa0d39cb0ec5210eb8329fd1bb6355d
kich555 <hyunjjang199582@gmail.com>
2022-09-12 02:34:49 +0900

    Create createTodo function on todo server model

--- client/app/models/todo.server.js
+++ client/app/models/todo.server.js
@@ -9,8 +9,16 @@ export async function updateTodos(todoList) {
     const newTodos =  Object.values(todoList).reduce((acc,cur)=> {
      return [...acc, ...cur]   
     })
-console.log('newTodos',newTodos)
-    return db.todo.updateMany({
-        
-    })
+    return db.todo.updateMany({})
+}
+
+export async function createTodo({ title, progress, category, index }) {
+    return db.todo.create({data: {
+        title,
+        description:'',
+        done: progress ==='done' ? true: false,
+        progress,
+        category,
+        index
+    }})
 }
