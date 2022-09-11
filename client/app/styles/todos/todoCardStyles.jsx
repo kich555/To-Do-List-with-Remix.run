5c2834c799a4e0b82ece3f633f8f3b1558538d85
kich555 <hyunjjang199582@gmail.com>
2022-09-11 19:39:35 +0900

    Refactoring todoCardStyles

--- client/app/styles/todos/todoCardStyles.jsx
+++ client/app/styles/todos/todoCardStyles.jsx
@@ -3,23 +3,20 @@ import { createStyles } from '@mantine/core';
 const todoCardStyles = createStyles(theme => ({
   item: {
     ...theme.fn.focusStyles(),
-    display: 'flex',
-    alignItems: 'center',
+
     borderRadius: theme.radius.md,
     border: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]}`,
-    padding: `${theme.spacing.sm}px ${theme.spacing.xl}px`,
+    overflow: 'hidden',
     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.white,
-    marginBottom: theme.spacing.sm,
+  },
+  inner: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
 
   itemDragging: {
     boxShadow: theme.shadows.sm,
-  },
-
-  symbol: {
-    fontSize: 30,
-    fontWeight: 700,
-    width: 60,
   },
 }));
 
