d356d1557a0520f1c071c34732887795cc888519
kich555 <hyunjjang199582@gmail.com>
2022-09-14 16:28:55 +0900

    add pointer Obj on commonStyles.js

--- client/app/styles/commonStyles.js
+++ client/app/styles/commonStyles.js
@@ -2,3 +2,4 @@ export const flexCol = { display: 'flex', flexDirection: 'column' }
 
 export const centered = { display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }
 
+export const pointer = { cursor: 'pointer'}
