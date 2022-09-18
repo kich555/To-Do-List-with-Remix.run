b329ec1faf065cc82b17441339710a23736d1b80
kich555 <hyunjjang199582@gmail.com>
2022-09-18 18:09:00 +0900

    Seperate route (login & register)

---
+++ client/app/routes/auth.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import { Outlet } from '@remix-run/react';
+import AuthModal from '~/components/pages/auth/AuthModal';
+
+export default function AuthRoute(params) {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setOpen(true), 0);
+  }, []);
+
+  return (
+    <>
+      <AuthModal open={open}>
+        <Outlet />
+      </AuthModal>
+    </>
+  );
+}
