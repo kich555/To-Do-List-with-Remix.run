e496e01f08468dd6848e210fdcde8cdc72855ddb
kich555 <hyunjjang199582@gmail.com>
2022-09-18 16:11:45 +0900

    Refactoring Layout component style

--- client/app/components/common/Layout.jsx
+++ client/app/components/common/Layout.jsx
@@ -3,6 +3,7 @@ import { createStyles } from '@mantine/core';
 const layoutStyles = createStyles(theme => ({
   wrapper: {
     position: 'relative',
+    minHeight: '100vh',
     backgroundImage: 'url(https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80)',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
@@ -13,5 +14,5 @@ export default function Layout({ children }) {
   const { classes } = layoutStyles();
   const { wrapper } = classes;
 
-  return <div className={wrapper}>{children}</div>;
+  return <section className={wrapper}>{children}</section>;
 }
