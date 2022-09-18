8f57bcd846e32daed085fb6789d097423ef64a45
kich555 <hyunjjang199582@gmail.com>
2022-09-18 18:09:30 +0900

    Create login route

---
+++ client/app/routes/auth/login.jsx
@@ -0,0 +1,16 @@
+import { Button, Input, Space } from '@mantine/core';
+import { Form } from '@remix-run/react';
+
+export default function LoginRoute(params) {
+  return (
+    <Form method="post">
+      <Space h={20} />
+      <Input name="id" sx={{ maxWidth: '240px' }} my={8} />
+      <Input name="passward" sx={{ maxWidth: '240px' }} my={8} />
+      <Button variant="gradient" sx={{ width: '100%', maxWidth: '240px' }} my={8}>
+        Log in
+      </Button>
+      <Space h={60} />
+    </Form>
+  );
+}
