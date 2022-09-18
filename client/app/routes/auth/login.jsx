8530d9175d58c82adff812348a9e14dad61181cb
kich555 <hyunjjang199582@gmail.com>
2022-09-18 19:20:50 +0900

    Creating login form action handler

--- client/app/routes/auth/login.jsx
+++ client/app/routes/auth/login.jsx
@@ -1,12 +1,55 @@
 import { Button, Input, Space } from '@mantine/core';
+import { json } from '@remix-run/node';
 import { Form } from '@remix-run/react';
+
+const badRequest = data => {
+  json(data, { status: 400 });
+};
+
+function validateUsername(username) {
+  if (typeof username !== 'string' || username.length < 3) {
+    return `Usernames must be at least 3 characters long`;
+  }
+}
+
+function validatePassword(password) {
+  if (typeof password !== 'string' || password.length < 6) {
+    return `Passwords must be at least 6 characters long`;
+  }
+}
+
+const validateStringInputType = ({ ...values }) => {
+  const arrayedObj = Object.keys(values);
+  arrayedObj.map(value => {
+    if (typeof value !== 'string') {
+      return badRequest({
+        formError: `${value}'s should be a string type`,
+      });
+    }
+  });
+};
+
+export const action = async ({ request }) => {
+  const formData = request.formData();
+  const values = Object.fromEntries(formData);
+  const { username, password } = values;
+  validateStringInputType(values);
+  const fieldErrors = {
+    username: validateUsername(username),
+    password: validatePassword(password),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, values });
+  }
+};
 
 export default function LoginRoute(params) {
   return (
     <Form method="post">
       <Space h={20} />
-      <Input name="id" sx={{ maxWidth: '240px' }} my={8} />
-      <Input name="passward" sx={{ maxWidth: '240px' }} my={8} />
+      <Input name="username" sx={{ maxWidth: '240px' }} my={8} />
+      <Input name="password" type="password" sx={{ maxWidth: '240px' }} my={8} />
       <Button variant="gradient" sx={{ width: '100%', maxWidth: '240px' }} my={8}>
         Log in
       </Button>
