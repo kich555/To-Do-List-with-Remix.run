6910106abf1325121620d4b6e8a64f7a696a8a85
kich555 <hyunjjang199582@gmail.com>
2022-09-19 18:56:16 +0900

    Creating register action handler

--- client/app/routes/auth/register.jsx
+++ client/app/routes/auth/register.jsx
@@ -1,7 +1,56 @@
 import { Button, Input, Space } from '@mantine/core';
+import { json, redirect } from '@remix-run/node';
 import { Form, useActionData } from '@remix-run/react';
 import authStyles from '~/pages/auth/styles/authStyles';
+import { badRequest, validateStringInputType } from '~/utils/actionHandler.server';
+import { checkUser } from '~/models/user.server';
+import { validateUsername, validatePassword } from '~/pages/auth/utils/authUtils';
 
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const fields = Object.fromEntries(formData);
+  const { username, password, confirmPassword } = fields;
+
+  // create confirm password field error message
+  const confirmPasswordFieldError = `Password did not match. Please try again.`;
+  // check password confirmed
+  if (password !== confirmPassword) {
+    const fieldErrors = {
+      confirmPassword: confirmPasswordFieldError,
+    };
+    return badRequest({ fieldErrors, fields });
+  }
+
+  // check input type is string
+  validateStringInputType(fields);
+
+  // create field error message
+  const fieldErrors = {
+    username: validateUsername(username),
+    password: validatePassword(password),
+    confirmPassword: confirmPasswordFieldError,
+  };
+
+  // check field has any error
+  const arrayedObj = Object.values(fieldErrors);
+  if (arrayedObj.some(Boolean)) {
+    return badRequest({ fieldErrors, fields });
+  }
+
+  // check user exist
+  const userExists = await checkUser(username);
+  if (userExists) {
+    return badRequest({
+      fields,
+      formError: `User with username ${username} already exists`,
+    });
+  }
+
+  // create the user
+  // create their session and redirect to /todos
+
+  return redirect('/auth/login');
+};
 
 export default function RegisterRoute(params) {
   const actionData = useActionData();
