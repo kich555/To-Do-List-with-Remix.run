08efdb03f82e3769cbe2397a3c9fe8dbfc7ee8ca
kich555 <hyunjjang199582@gmail.com>
2022-09-20 19:48:12 +0900

    Apply Errorhandler on auth route

--- client/app/routes/auth.jsx
+++ client/app/routes/auth.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Outlet } from '@remix-run/react';
 import AuthModal from '~/pages/auth/AuthModal';
+import AuthErrorContainer from '~/pages/auth/errors/AuthErrorContainer';
 
 export default function AuthRoute(params) {
   const [open, setOpen] = useState(false);
@@ -15,5 +16,13 @@ export default function AuthRoute(params) {
         <Outlet />
       </AuthModal>
     </>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <AuthModal open={true}>
+      <AuthErrorContainer message={error?.message} status={error?.status} />
+    </AuthModal>
   );
 }
