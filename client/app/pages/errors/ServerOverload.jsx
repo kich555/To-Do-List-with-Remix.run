fe3060785863b182e9b303d50d2a423275c02e11
kich555 <hyunjjang199582@gmail.com>
2022-09-20 20:53:17 +0900

    Debugging ServerOverload component (handle when error obj is null)

--- client/app/pages/errors/ServerOverload.jsx
+++ client/app/pages/errors/ServerOverload.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation, useNavigate } from '@remix-run/react';
 import serverOverloadStyles from './styles/serverOverloadStyles';
 
 export default function ServerOverload({ error }) {
-  const { status, message, statusText } = error;
+  // const { status, message, statusText } = error;
   const { pathname } = useLocation();
 
   const { classes } = serverOverloadStyles(pathname);
@@ -15,17 +15,17 @@ export default function ServerOverload({ error }) {
   };
 
   const errorDescription = () => {
-    if (message) {
-      return message;
+    if (error?.message) {
+      return error.message;
     }
-    if (statusText) {
+    if (error?.statusText) {
       return '';
     }
     return 'Our servers could not handle your request. Don&apos;t worry, our development team was already notified. Try refreshing the page.';
   };
 
   const ErrorButton = () => {
-    if (status === 404) {
+    if (error?.status === 404) {
       return (
         <Button component={Link} to="/" variant="white" size="md" className={button} mb={36}>
           Back to main
@@ -43,10 +43,10 @@ export default function ServerOverload({ error }) {
     <Box className={root}>
       <Container>
         <Text align="center" weight={900} className={label}>
-          {status || 500}
+          {error?.status || 500}
         </Text>
         <Title align="center" color="white" weight={900} className={title} mt={36}>
-          {statusText || 'Something bad just happened...'}
+          {error?.statusText || 'Something bad just happened...'}
         </Title>
         <Text size="lg" align="center" color="white" mt={24} className={description}>
           {errorDescription}
