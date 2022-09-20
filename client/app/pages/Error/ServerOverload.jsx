eedc2b674d085be09654627e9bcfd809a093a13a
kich555 <hyunjjang199582@gmail.com>
2022-09-20 17:24:09 +0900

    Add props on ServerOverload component

--- client/app/pages/Error/ServerOverload.jsx
+++ client/app/pages/Error/ServerOverload.jsx
@@ -1,23 +1,29 @@
 import { Box, Title, Text, Button, Container, Group } from '@mantine/core';
+import { useNavigate } from '@remix-run/react';
 import serverOverloadStyles from './styles/serverOverloadStyles';
 
-export default function ServerOverload() {
+export default function ServerOverload({ status, message }) {
   const { theme, classes } = serverOverloadStyles();
   const { root, label, title, description } = classes;
+  const navigate = useNavigate();
+
+  const handleRefresh = () => {
+    navigate('.', { replace: true });
+  };
   return (
     <Box className={root}>
       <Container>
         <Text align="center" weight={900} className={label}>
-          500
+          {status || 500}
         </Text>
         <Title align="center" color="white" weight={900} className={title} mt={36}>
           Something bad just happened...
         </Title>
         <Text size="lg" align="center" color="white" mt={24} className={description}>
-          Our servers could not handle your request. Don&apos;t worry, our development team was already notified. Try refreshing the page.
+          {message || 'Our servers could not handle your request. Don&apos;t worry, our development team was already notified. Try refreshing the page.'}
         </Text>
         <Group position="center" mt={36}>
-          <Button variant="white" size="md">
+          <Button type="button" variant="white" size="md" onClick={handleRefresh}>
             Refresh the page
           </Button>
         </Group>
