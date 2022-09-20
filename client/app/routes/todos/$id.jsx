8b244d2985e417870b691981ec9015e79cbf3e81
kich555 <hyunjjang199582@gmail.com>
2022-09-20 18:53:43 +0900

    Seperate Layout part from todos/ route

--- client/app/routes/todos/$id.jsx
+++ client/app/routes/todos/$id.jsx
@@ -1,10 +1,7 @@
-import { Badge, Box, Button, Container, Divider, Title, Input } from '@mantine/core';
 import { json, redirect } from '@remix-run/node';
-import { Form, useLoaderData } from '@remix-run/react';
-import { useState } from 'react';
+import { useLoaderData } from '@remix-run/react';
 import { getSingleTodo, deleteTodo, updateSingleTodo } from '~/models/todo.server';
-import UpdateDescriptionContainer from '~/pages/todo/UpdateDescriptionContainer';
-import DefaultDescriptionContainer from '~/pages/todo/DefaultDescriptionContainer';
+import TodoDetail from '~/pages/todo/TodoDetail';
 
 export const loader = async ({ params }) => {
   const { id } = params;
@@ -27,42 +24,7 @@ export const action = async ({ request }) => {
 };
 
 export default function $idRoute() {
-  const { id, title, category, description } = useLoaderData();
-  const [todoCategory, setTodoCategory] = useState(category);
-  const [editDescription, setEditDescription] = useState(false);
+  const loaderData = useLoaderData();
 
-  const badge = {
-    cursor: 'pointer',
-  };
-
-  return (
-    <Container>
-      <Title align="center">{title}</Title>
-      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }} pt={12}>
-        <Form method="patch">
-          <Badge size="lg" component="button" sx={badge} variant={todoCategory === 'life' ? 'filled' : 'light'} onClick={() => setTodoCategory('life')}>
-            life
-          </Badge>
-          <Badge size="lg" component="button" sx={badge} color="teal" variant={todoCategory === 'work' ? 'filled' : 'light'} ml={8} onClick={() => setTodoCategory('work')}>
-            work
-          </Badge>
-        </Form>
-      </Box>
-      <Divider mt={24} />
-      {editDescription ? (
-        <UpdateDescriptionContainer id={id} todoCategory={todoCategory} setEditDescription={setEditDescription} />
-      ) : (
-        <DefaultDescriptionContainer description={description} setEditDescription={setEditDescription} />
-      )}
-      <Divider mt={24} />
-      <Form method="delete">
-        <Box sx={{ display: 'flex', justifyContent: 'end' }}>
-          <Input type="hidden" name="_id" value={id} />
-          <Button type="submit" name="_action" value="delete" mt={20} color="red" aria-label="delete">
-            Delete
-          </Button>
-        </Box>
-      </Form>
-    </Container>
-  );
+  return <TodoDetail loaderData={loaderData} />;
 }
