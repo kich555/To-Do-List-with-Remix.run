adadb699af97b32d9fe8352c6a3393474c9ed58d
kich555 <hyunjjang199582@gmail.com>
2022-09-20 19:16:50 +0900

    Create category update action handler on todos/

--- client/app/routes/todos/$id.jsx
+++ client/app/routes/todos/$id.jsx
@@ -1,7 +1,9 @@
 import { json, redirect } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
+
 import { getSingleTodo, deleteTodo, updateSingleTodo } from '~/models/todo.server';
 import TodoDetail from '~/pages/todo/TodoDetail';
+import TodoErrorContainer from '~/pages/todo/errors/TodoErrorContainer';
 
 export const loader = async ({ params }) => {
   const { id } = params;
@@ -11,20 +13,30 @@ export const loader = async ({ params }) => {
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
-  const { _action, ...values } = Object.fromEntries(formData);
+  const { _action, _id, ...values } = Object.fromEntries(formData);
 
   if (_action === 'update') {
-    await updateSingleTodo({ ...values });
-    return redirect(`/todos/${values._id}`);
+    await updateSingleTodo({ _id, ...values });
+    return redirect(`/todos/${_id}`);
   }
   if (_action === 'delete') {
-    await deleteTodo({ ...values });
+    await deleteTodo({ _id });
     return redirect('/todos');
+  }
+
+  if (_action === 'categoryUpdate') {
+    await updateSingleTodo({ _id, ...values });
+    return redirect(`/todos/${_id}`);
   }
 };
 
 export default function $idRoute() {
   const loaderData = useLoaderData();
-
+  throw new Error('Testing Error Boundary');
   return <TodoDetail loaderData={loaderData} />;
+}
+
+export function ErrorBoundary({ error }) {
+  console.log('error', error.status);
+  return <TodoErrorContainer message={error?.message} status={error?.status} />;
 }
