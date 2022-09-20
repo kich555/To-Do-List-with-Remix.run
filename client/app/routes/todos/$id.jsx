538848af419c241bfaa3591276e27b301176d15f
kich555 <hyunjjang199582@gmail.com>
2022-09-20 18:47:13 +0900

    Refactoring todo detail router

--- client/app/routes/todos/$id.jsx
+++ client/app/routes/todos/$id.jsx
@@ -1,9 +1,10 @@
-import { Badge, Box, Button, Container, Divider, Textarea, Title, Group, Input } from '@mantine/core';
+import { Badge, Box, Button, Container, Divider, Title, Input } from '@mantine/core';
 import { json, redirect } from '@remix-run/node';
-import { Form, useLoaderData, useSubmit } from '@remix-run/react';
+import { Form, useLoaderData } from '@remix-run/react';
 import { useState } from 'react';
 import { getSingleTodo, deleteTodo, updateSingleTodo } from '~/models/todo.server';
-import todoDetailStyles from '~/pages/todos/styles/todoDetailStyles';
+import UpdateDescriptionContainer from '~/pages/todo/UpdateDescriptionContainer';
+import DefaultDescriptionContainer from '~/pages/todo/DefaultDescriptionContainer';
 
 export const loader = async ({ params }) => {
   const { id } = params;
@@ -27,72 +28,32 @@ export const action = async ({ request }) => {
 
 export default function $idRoute() {
   const { id, title, category, description } = useLoaderData();
-  const submit = useSubmit();
   const [todoCategory, setTodoCategory] = useState(category);
   const [editDescription, setEditDescription] = useState(false);
-  const { classes, cx } = todoDetailStyles();
-  const { formWrapper, descriptionBox, editBox, updateBox, buttonGroup, badge } = classes;
 
-  const handleUpdate = event => {
-    const formData = new FormData(event.currentTarget);
-    setEditDescription(false);
-    return submit(formData, { method: 'patch' });
+  const badge = {
+    cursor: 'pointer',
   };
-
-  function DefaultDescriptionBox() {
-    if (description) {
-      return (
-        <Box mt={24} className={cx(formWrapper, descriptionBox)} onClick={() => setEditDescription(true)}>
-          {description}
-          <Button>Edit</Button>
-        </Box>
-      );
-    } else {
-      return (
-        <Box mt={24} className={cx(formWrapper, editBox)}>
-          <Button onClick={() => setEditDescription(true)}>Edit</Button>
-        </Box>
-      );
-    }
-  }
-
-  function EditDescriptionBox() {
-    return (
-      <Box mt={24} className={cx(formWrapper, updateBox)}>
-        <Form method="patch" onSubmit={handleUpdate}>
-          <Box className={updateBox}>
-            <Input type="hidden" name="_id" value={id} />
-            <Input type="hidden" name="category" value={todoCategory} />
-            <Textarea autoFocus autosize minRows={12} variant="unstyled" name="description" />
-            <Group spacing={10} noWrap={true} mt={20} className={buttonGroup}>
-              <Button type="submit" name="_action" value="update">
-                Save
-              </Button>
-              <Button type="button" color="red" onClick={() => setEditDescription(false)}>
-                Cancel
-              </Button>
-            </Group>
-          </Box>
-        </Form>
-      </Box>
-    );
-  }
 
   return (
     <Container>
       <Title align="center">{title}</Title>
       <Box sx={{ display: 'flex', justifyContent: 'flex-end' }} pt={12}>
         <Form method="patch">
-          <Badge size="lg" component="button" variant={todoCategory === 'life' ? 'filled' : 'light'} className={badge} onClick={() => setTodoCategory('life')}>
+          <Badge size="lg" component="button" sx={badge} variant={todoCategory === 'life' ? 'filled' : 'light'} onClick={() => setTodoCategory('life')}>
             life
           </Badge>
-          <Badge size="lg" component="button" color="teal" variant={todoCategory === 'work' ? 'filled' : 'light'} ml={8} className={badge} onClick={() => setTodoCategory('work')}>
+          <Badge size="lg" component="button" sx={badge} color="teal" variant={todoCategory === 'work' ? 'filled' : 'light'} ml={8} onClick={() => setTodoCategory('work')}>
             work
           </Badge>
         </Form>
       </Box>
       <Divider mt={24} />
-      {editDescription ? <EditDescriptionBox /> : <DefaultDescriptionBox />}
+      {editDescription ? (
+        <UpdateDescriptionContainer id={id} todoCategory={todoCategory} setEditDescription={setEditDescription} />
+      ) : (
+        <DefaultDescriptionContainer description={description} setEditDescription={setEditDescription} />
+      )}
       <Divider mt={24} />
       <Form method="delete">
         <Box sx={{ display: 'flex', justifyContent: 'end' }}>
@@ -102,7 +63,6 @@ export default function $idRoute() {
           </Button>
         </Box>
       </Form>
-      {/* <Box >{todoCategory}</Box> */}
     </Container>
   );
 }
