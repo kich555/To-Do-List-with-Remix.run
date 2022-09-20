8fd013b7ac7d03952dd144fa60b6da6c631803ec
kich555 <hyunjjang199582@gmail.com>
2022-09-20 21:16:19 +0900

    Seperate business logic in TodoList component (dnd logic)

---
+++ client/app/pages/todo/controller/useDropList.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useCallback, useState, useRef, useMemo } from 'react';
+import { resetServerContext } from 'react-beautiful-dnd';
+import { removeFormList, addToList, objectToArray } from './utils/handleArray';
+
+export default function useDropList({ progress, todos }) {
+  const [todoList, setTodoList] = useState(todos);
+  const dropFormRef = useRef();
+  resetServerContext();
+
+  const arrayedTodoList = useMemo(() => {
+    return objectToArray(todoList);
+  }, [todoList]);
+
+  const handleDrop = useCallback(() => {
+    if (todos === todoList) return;
+    dropFormRef.current?.submit();
+  }, [todos, todoList]);
+
+  const handleDragEnd = useCallback(
+    ({ destination, source }) => {
+      if (!destination) return;
+      if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+      const copiedList = { ...todoList };
+
+      //드래그 시작한 카테고리 리스트
+      const sourceCategory = copiedList[source.droppableId];
+      // 드래그 한 카드를 삭제한 새 배열 반환 + 삭제한 카드 추출
+      const [newSourceCategory, removedCard] = removeFormList(sourceCategory, source.index);
+      // 기존 배열을 새 배열로 대체
+      copiedList[source.droppableId] = newSourceCategory;
+      //드래그가 끝난 카테고리 리스트
+      const destinationCategory = copiedList[destination.droppableId];
+      // 드래그한 아이템을 기존 배열에 추가
+      copiedList[destination.droppableId] = addToList(destinationCategory, destination.index, removedCard);
+      // 드래그한 아이템의 progress 상태 변경
+      removedCard.progress = destination.droppableId;
+      // 아이템들의 index값 변경
+      progress.map(category => copiedList[category].map((item, currentIndex) => (item.index = currentIndex)));
+
+      setTodoList(copiedList);
+    },
+    [progress, todoList],
+  );
+
+  useEffect(() => {
+    setTodoList(todos);
+  }, [todos]);
+
+  useEffect(() => {
+    handleDrop();
+  }, [handleDrop]);
+
+  return [todoList, arrayedTodoList, handleDragEnd];
+}
--- client/app/pages/todos/TodoList.jsx
+++ client/app/pages/todos/TodoList.jsx
@@ -1,101 +1,14 @@
-import { useEffect, useCallback, useState, useRef, useMemo } from 'react';
+import { useRef, useMemo } from 'react';
 import { Form } from '@remix-run/react';
 import { Box, Input, Button, Title } from '@mantine/core';
-import { DragDropContext, resetServerContext } from 'react-beautiful-dnd';
+import { DragDropContext } from 'react-beautiful-dnd';
 import TodoProgress from './TodoProgress';
-
-/**
-    * @author Kich555 <hyunjjang199582@gmail.com>
-원래라면 순수함수들은 utils directory에 모듈로 따로 빼두거나 lodash/es등을 사용하겠지만,
-개인 미니 프로젝트 + 다른 곳에서 재사용 할 예정이 없는 순수 함수 특성 상
- 응집도를 위해 TodoList.jsx에 선언 해둠
-   */
-const removeFormList = (list, index) => {
-  const result = [...list];
-  const [removed] = result.splice(index, 1);
-  return [result, removed];
-};
-
-/**
-    * @author Kich555 <hyunjjang199582@gmail.com>
-원래라면 순수함수들은 utils directory에 모듈로 따로 빼두거나 lodash/es등을 사용하겠지만,
-개인 미니 프로젝트 + 다른 곳에서 재사용 할 예정이 없는 순수 함수 특성 상
- 응집도를 위해 TodoList.jsx에 선언 해둠
-   */
-const addToList = (list, index, element) => {
-  const result = [...list];
-  result.splice(index, 0, element);
-  return result;
-};
-
-/**
-    * @author Kich555 <hyunjjang199582@gmail.com>
-원래라면 순수함수들은 utils directory에 모듈로 따로 빼두거나 lodash/es등을 사용하겠지만,
-개인 미니 프로젝트 + 다른 곳에서 재사용 할 예정이 없는 순수 함수 특성 상
- 응집도를 위해 TodoList.jsx에 선언 해둠
-   */
-const objectToArray = obj => {
-  const arrayedObject = Object.values(obj);
-
-  if (arrayedObject.length === 0) {
-    return arrayedObject;
-  } else {
-    arrayedObject.reduce((acc, cur) => {
-      return [...acc, ...cur];
-    });
-  }
-
-  return arrayedObject;
-};
+import useDropList from '../todo/controller/useDropList';
 
 export default function TodoList({ user, todos }) {
-  const [todoList, setTodoList] = useState(todos);
   const dropFormRef = useRef();
-  resetServerContext();
   const progress = useMemo(() => ['todo', 'inProgress', 'done'], []);
-  const arrayedTodoList = useMemo(() => {
-    return objectToArray(todoList);
-  }, [todoList]);
-
-  const handleDrop = useCallback(() => {
-    if (todos === todoList) return;
-    dropFormRef.current?.submit();
-  }, [todos, todoList]);
-
-  const handleDragEnd = useCallback(
-    ({ destination, source }) => {
-      if (!destination) return;
-      if (destination.droppableId === source.droppableId && destination.index === source.index) return;
-
-      const copiedList = { ...todoList };
-
-      //드래그 시작한 카테고리 리스트
-      const sourceCategory = copiedList[source.droppableId];
-      // 드래그 한 카드를 삭제한 새 배열 반환 + 삭제한 카드 추출
-      const [newSourceCategory, removedCard] = removeFormList(sourceCategory, source.index);
-      // 기존 배열을 새 배열로 대체
-      copiedList[source.droppableId] = newSourceCategory;
-      //드래그가 끝난 카테고리 리스트
-      const destinationCategory = copiedList[destination.droppableId];
-      // 드래그한 아이템을 기존 배열에 추가
-      copiedList[destination.droppableId] = addToList(destinationCategory, destination.index, removedCard);
-      // 드래그한 아이템의 progress 상태 변경
-      removedCard.progress = destination.droppableId;
-      // 아이템들의 index값 변경
-      progress.map(category => copiedList[category].map((item, currentIndex) => (item.index = currentIndex)));
-
-      setTodoList(copiedList);
-    },
-    [progress, todoList],
-  );
-
-  useEffect(() => {
-    setTodoList(todos);
-  }, [todos]);
-
-  useEffect(() => {
-    handleDrop();
-  }, [handleDrop]);
+  const [todoList, arrayedTodoList, handleDragEnd] = useDropList({ progress, todos });
 
   return (
     <>
