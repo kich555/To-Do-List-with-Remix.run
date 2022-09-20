57ff9164d18fe621cb190dfd64005735644fcd5b
kich555 <hyunjjang199582@gmail.com>
2022-09-21 01:52:46 +0900

    Add CatchBoundary on root.jsx

--- client/app/root.jsx
+++ client/app/root.jsx
@@ -1,4 +1,4 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useCatch } from '@remix-run/react';
 import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
 import { StylesPlaceholder } from '@mantine/remix';
@@ -49,6 +49,16 @@ export default function App() {
         <Scripts />
         <LiveReload />
       </NotificationsProvider>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <Document title={`${caught.status} || ${caught.statusText}`}>
+      <DefaultErrorContainer error={caught} />
     </Document>
   );
 }
