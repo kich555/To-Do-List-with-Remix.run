4717e3e210fb936d5f4080f031174e10055bb9b1
kich555 <hyunjjang199582@gmail.com>
2022-09-21 02:14:15 +0900

    Refactoring /todos/id error handler

--- client/app/routes/todos/$id.jsx
+++ client/app/routes/todos/$id.jsx
@@ -1,5 +1,5 @@
 import { json, redirect } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { useCatch, useLoaderData, useParams } from '@remix-run/react';
 
 import { getSingleTodo, deleteTodo, updateSingleTodo } from '~/models/todo.server';
 import TodoDetail from '~/pages/todo/TodoDetail';
@@ -8,6 +8,13 @@ import TodoErrorContainer from '~/pages/todo/errors/TodoErrorContainer';
 export const loader = async ({ params }) => {
   const { id } = params;
   const todo = await getSingleTodo(id);
+
+  if (!todo) {
+    throw new Response('Todo Not Found', {
+      status: 404,
+      statusText: `Todo Not Found`,
+    });
+  }
   return json(todo);
 };
 
@@ -32,11 +39,19 @@ export const action = async ({ request }) => {
 
 export default function $idRoute() {
   const loaderData = useLoaderData();
-  throw new Error('Testing Error Boundary');
+  // throw new Error('Testing Error Boundary');
   return <TodoDetail loaderData={loaderData} />;
 }
 
+export function CatchBoundary() {
+  const caught = useCatch();
+  const params = useParams();
+  if (caught.status === 404) {
+    return <TodoErrorContainer error={{ message: `Todo id : ${params.id} does not exist.`, ...caught }} />;
+  }
+  throw new Error(`Unhandled error: ${caught.status}`);
+}
+
 export function ErrorBoundary({ error }) {
-  console.log('error', error.status);
-  return <TodoErrorContainer message={error?.message} status={error?.status} />;
+  return <TodoErrorContainer error={error} />;
 }
