aafb3f1288129f4947041d6540b15305cba01cc1
kich555 <hyunjjang199582@gmail.com>
2022-09-23 05:38:29 +0900

    Refactoring login route

--- client/app/routes/auth/login.jsx
+++ client/app/routes/auth/login.jsx
@@ -2,8 +2,10 @@ import { Form, useActionData } from '@remix-run/react';
 import { login, createUserSession } from '~/utils/session.server';
 import { Button, Input, Space } from '@mantine/core';
 import authStyles from '~/pages/auth/styles/authStyles';
-import { validateUsername, validatePassword } from '~/pages/auth/utils/authUtils';
+import { validateUsername, validatePassword } from '~/pages/auth/controller/utils/authUtils';
 import { badRequest, validateStringInputType } from '~/utils/actionHandler.server';
+import { useAuthUX } from '~/pages/auth/controller/context/AuthUXProvider';
+import { useEffect } from 'react';
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
@@ -34,6 +36,7 @@ export const action = async ({ request }) => {
       formError: `Username/Password combination is incorrect`,
     });
   }
+  console.log('user', user);
   return createUserSession(user.id, redirectTo);
 };
 
@@ -41,11 +44,16 @@ export default function LoginRoute() {
   const actionData = useActionData();
   const { classes } = authStyles();
   const { label, input, errorInput, errorMessage, button } = classes;
+  const [, , setActionData] = useAuthUX();
+
+  useEffect(() => {
+    if (!actionData) return;
+    setActionData(actionData);
+  }, [setActionData, actionData]);
 
   return (
     <Form method="post">
       <Space h={20} />
-
       <Input.Label htmlFor="username-input" mt={4} className={label}>
         <Input
           id="username-input"
@@ -53,13 +61,13 @@ export default function LoginRoute() {
           type="text"
           defaultValue={actionData?.fields?.username}
           aria-label="username"
-          aria-invalid={Boolean(actionData?.fieldErrors?.username)}
-          aria-errormessage={actionData?.fieldErrors?.username && 'username-error'}
+          aria-invalid={Boolean(actionData?.fieldErrors?.username) || Boolean(actionData?.formError)}
+          aria-errormessage={(actionData?.fieldErrors?.username && 'username-error') || (actionData?.formError && 'User does not exist.')}
           placeholder="username"
           className={input}
-          styles={{ input: actionData?.fieldErrors?.username && errorInput }}
+          styles={{ input: (actionData?.fieldErrors?.username || actionData?.formError) && errorInput }}
         />
-        <Input.Error className={errorMessage}>{actionData?.fieldErrors?.username} </Input.Error>
+        <Input.Error className={errorMessage}>{actionData?.fieldErrors?.username || actionData?.formError} </Input.Error>
       </Input.Label>
       <Input.Label htmlFor="password-input" mt={4} className={label}>
         <Input
@@ -68,13 +76,13 @@ export default function LoginRoute() {
           type="password"
           defaultValue={actionData?.fields?.password}
           aria-label="password"
-          aria-invalid={Boolean(actionData?.fieldErrors?.password)}
-          aria-errormessage={actionData?.fieldErrors?.password && 'password-error'}
+          aria-invalid={Boolean(actionData?.fieldErrors?.password) || Boolean(actionData?.formError)}
+          aria-errormessage={(actionData?.fieldErrors?.password && 'password-error') || (actionData?.formError && 'User does not exist.')}
           placeholder="password"
           className={input}
-          styles={{ input: actionData?.fieldErrors?.password && errorInput }}
+          styles={{ input: (actionData?.fieldErrors?.password || actionData?.formError) && errorInput }}
         />
-        <Input.Error className={errorMessage}>{actionData?.fieldErrors?.password} </Input.Error>
+        <Input.Error className={errorMessage}>{actionData?.fieldErrors?.password || actionData?.formError} </Input.Error>
       </Input.Label>
       <Button type="submit" variant="gradient" className={button} mt={8}>
         Log in
