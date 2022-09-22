5f54b987130a45b939ff1687848b2bd429de2350
kich555 <hyunjjang199582@gmail.com>
2022-09-23 05:43:36 +0900

    Debugging useDropList

--- client/app/pages/todo/controller/hook/useDropList.jsx
+++ client/app/pages/todo/controller/hook/useDropList.jsx
@@ -16,20 +16,21 @@ export function useDropList({ progress, todos }) {
 
       const copiedList = { ...todoList };
 
-      //드래그 시작한 카테고리 리스트
+      //드래그 한 카테고리 리스트
       const sourceCategory = copiedList[source.droppableId];
       // 드래그 한 카드를 삭제한 새 배열 반환 + 삭제한 카드 추출
       const [newSourceCategory, removedCard] = removeFormList(sourceCategory, source.index);
       // 기존 배열을 새 배열로 대체
       copiedList[source.droppableId] = newSourceCategory;
-      //드래그가 끝난 카테고리 리스트
-      const destinationCategory = copiedList[destination.droppableId];
+      //드롭 할 카테고리 리스트
+      const destinationCategory = copiedList[destination.droppableId] || [];
       // 드래그한 아이템을 기존 배열에 추가
       copiedList[destination.droppableId] = addToList(destinationCategory, destination.index, removedCard);
       // 드래그한 아이템의 progress 상태 변경
       removedCard.progress = destination.droppableId;
       // 아이템들의 index값 변경
-      progress.map(category => copiedList[category].map((item, currentIndex) => (item.index = currentIndex)));
+
+      progress?.map(category => copiedList[category]?.map((item, currentIndex) => (item.index = currentIndex)));
 
       setTodoList(copiedList);
     },
