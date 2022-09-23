76e18b7913181a97b210d8a48b43860d99c74a79
kich555 <hyunjjang199582@gmail.com>
2022-09-23 23:52:08 +0900

    [Migration To TS] auth route

--- client/app/routes/auth.tsx
+++ client/app/routes/auth.tsx
@@ -2,6 +2,7 @@ import { Outlet, useCatch } from '@remix-run/react';
 import { AuthUXProvider } from '~/pages/auth/controller/context/AuthUXProvider';
 import AuthModal from '~/pages/auth/AuthModal';
 import AuthErrorContainer from '~/pages/auth/errors/AuthErrorContainer';
+import { CaughtError } from '~/types/commontypes';
 
 export default function AuthRoute() {
   return (
@@ -19,11 +20,11 @@ export function CatchBoundary() {
   const caught = useCatch();
 }
 
-export function ErrorBoundary({ error }) {
+export function ErrorBoundary({ error }: { error: CaughtError }) {
   return (
     <AuthUXProvider>
       <AuthModal>
-        <AuthErrorContainer message={error?.message} status={error?.status} />
+        <AuthErrorContainer error={error} />
       </AuthModal>
     </AuthUXProvider>
   );
