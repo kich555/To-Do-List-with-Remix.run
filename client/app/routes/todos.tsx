086961a2b18af7e92059727460078ab97efc3e88
kich555 <hyunjjang199582@gmail.com>
2022-09-25 23:50:54 +0900

    [Migration To TS] todos route

--- client/app/routes/todos.tsx
+++ client/app/routes/todos.tsx
@@ -1,23 +1,32 @@
 import { useLoaderData } from '@remix-run/react';
-import { json, redirect } from '@remix-run/node';
+import { ActionArgs, ActionFunction, json, redirect } from '@remix-run/node';
 import { createTodo, getUserTodos, updateUserTodos } from '~/models/todo.server';
 import { requireUserId, getUser } from '~/utils/session.server';
 import TodoList from '~/pages/todos/TodoList';
 import ListErrorContainer from '~/pages/todos/errors/ListErrorContainer';
 import { TodoModalProvider } from '~/pages/todo/controller/context/TodoModalProvider';
 
-export const loader = async ({ request }) => {
+import type { LoaderArgs, LoaderFunction } from '@remix-run/node';
+import type { Todo } from '@prisma/client';
+import invariant from 'tiny-invariant';
+import { CaughtError } from '~/types/commontypes';
+
+interface NewTodos {
+  [process: string]: Todo[];
+}
+
+export const loader: LoaderFunction = async ({ request }: LoaderArgs) => {
   const user = await getUser(request);
 
   // If user is undifined redirect to /auth/login
   if (!user) return redirect('/auth/login');
   const todos = await getUserTodos(user);
 
-  const getNewTodos = () => {
+  function getNewTodos(): NewTodos {
     if (todos.length === 0) {
-      return [];
+      return {};
     } else {
-      return todos?.reduce((acc, cur) => {
+      return todos?.reduce((acc: NewTodos, cur) => {
         if (!acc[cur.progress]) {
           acc[cur.progress] = [cur];
         } else {
@@ -28,22 +37,27 @@ export const loader = async ({ request }) => {
         return { ...acc };
       }, {});
     }
-  };
+  }
 
   const newTodos = getNewTodos();
 
   return json({ user, newTodos });
 };
 
-export const action = async ({ request }) => {
+export const action: ActionFunction = async ({ request }: ActionArgs) => {
   const userId = await requireUserId(request);
   const formData = await request.formData();
 
   const { _action, ...values } = Object.fromEntries(formData);
 
+  invariant(typeof _action === 'string', `${_action} must be a string`);
+
+  // invariant(typeof values.index === 'string', `Todo must be a object`);
+
   if (_action === 'create') {
     // values.index type 변경
-    values.index *= 1;
+
+    // values.index *= 1; <--잠시 꺼둠
 
     const { title } = values;
 
@@ -52,14 +66,18 @@ export const action = async ({ request }) => {
     }
 
     await createTodo({ userId, ...values });
-    return redirect('/todos');
+    // return redirect('/todos');
   }
 
   if (_action === 'drop') {
-    const todos = JSON.parse(formData.get('todos'));
-    await updateUserTodos(todos);
-    return redirect('/todos');
+    const todos = formData.get('todos');
+
+    invariant(typeof todos === 'string', `${todos} must be a array`);
+
+    const parsedTodos: Todo[] = JSON.parse(todos);
+    await updateUserTodos(parsedTodos);
   }
+  return redirect('/todos');
 };
 
 export default function TodosIndexRoute() {
@@ -71,6 +89,6 @@ export default function TodosIndexRoute() {
   );
 }
 
-export function ErrorBoundary({ error }) {
+export function ErrorBoundary({ error }: { error: CaughtError }) {
   return <ListErrorContainer error={error} />;
 }
