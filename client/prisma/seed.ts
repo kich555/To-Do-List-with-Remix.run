a9270cd064a78ae85c99390fa40395a7eb734a29
kich555 <hyunjjang199582@gmail.com>
2022-09-27 04:28:35 +0900

    Modify Todo schema

--- client/prisma/seed.ts
+++ client/prisma/seed.ts
@@ -25,7 +25,7 @@ function getTodos() {
     {
       title: 'Road worker',
       description: `I never wanted to believe that my Dad was stealing from his job as a road worker. But when I got home, all the signs were there.`,
-      done: false,
+
       category: 'work',
       progress: 'todo',
       index: 0,
@@ -33,7 +33,7 @@ function getTodos() {
     {
       title: 'Frisbee',
       description: `I was wondering why the frisbee was getting bigger, then it hit me.`,
-      done: false,
+
       category: 'life',
       progress: 'todo',
       index: 1,
@@ -41,7 +41,6 @@ function getTodos() {
     {
       title: 'Trees',
       description: `Why do trees seem suspicious on sunny days? Dunno, they're just a bit shady.`,
-      done: false,
       category: 'life',
       progress: 'todo',
       index: 2,
@@ -49,7 +48,6 @@ function getTodos() {
     {
       title: 'Skeletons',
       description: `Why don't skeletons ride roller coasters? They don't have the stomach for it.`,
-      done: false,
       category: 'work',
       progress: 'inProgress',
       index: 0,
@@ -57,7 +55,6 @@ function getTodos() {
     {
       title: 'Hippos',
       description: `Why don't you find hippopotamuses hiding in trees? They're really good at it.`,
-      done: false,
       category: 'life',
       progress: 'inProgress',
       index: 1,
@@ -65,7 +62,6 @@ function getTodos() {
     {
       title: 'Dinner',
       description: `What did one plate say to the other plate? Dinner is on me!`,
-      done: false,
       category: 'life',
       progress: 'done',
       index: 0,
@@ -73,7 +69,6 @@ function getTodos() {
     {
       title: 'Elevator',
       description: `My first time using an elevator was an uplifting experience. The second time let me down.`,
-      done: false,
       category: 'work',
       progress: 'done',
       index: 1,
