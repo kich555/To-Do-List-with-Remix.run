52a21718ffaa8b3968c46d75fb1d63dd652794b9
kich555 <hyunjjang199582@gmail.com>
2022-09-27 04:29:23 +0900

    [Migration To TS] Modify autUtil validation function return value

--- client/app/pages/auth/controller/utils/authUtils.ts
+++ client/app/pages/auth/controller/utils/authUtils.ts
@@ -2,14 +2,14 @@ export function validateUsername(username: string) {
   if (typeof username !== 'string' || username.length < 3) {
     return `Usernames must be at least 3 characters long`;
   }
-  return undefined;
+  return '';
 }
 
 export function validatePassword(password: string) {
   if (typeof password !== 'string' || password.length < 6) {
     return `Passwords must be at least 6 characters long`;
   }
-  return undefined;
+  return '';
 }
 
 export function capitalize(string: string) {
